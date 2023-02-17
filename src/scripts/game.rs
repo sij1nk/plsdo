dfcabf4fdbf8b376f468480d31aa5633ceec234b
anguaive <angurg98@gmail.com>
2023-02-17 18:46:40 +0100

    game: Sort results by name

--- src/scripts/game.rs
+++ src/scripts/game.rs
@@ -6,10 +6,11 @@ use crate::util::dmenu;
 pub fn run(sh: &Shell, _: &ArgMatches) -> anyhow::Result<()> {
 
     let list_output = cmd!(sh, "lutris -l").ignore_stderr().read()?;
-    let choices = list_output
+    let mut choices = list_output
         .split('\n')
         .map(|s| s.split('|').take(2).collect::<Vec<_>>().join("|"))
         .collect::<Vec<_>>();
+    choices.sort();
 
     // unwrap: we don't want to continue if result is empty
     let result = dmenu(sh, "Choose game", &choices, true).unwrap();
