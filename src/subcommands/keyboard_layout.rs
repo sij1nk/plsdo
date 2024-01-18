6c3f3fa4ea59837437aa5c2cb649c00c02002ee3
sij1nk <gergo.rajnavolgyi@gmail.com>
2024-01-18 21:42:58 +0100

    Get keyboard layout names on Hyprland

--- src/subcommands/keyboard_layout.rs
+++ src/subcommands/keyboard_layout.rs
@@ -1,16 +1,60 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
 use clap::{ArgMatches, Command};
 
 use xshell::Shell;
 
-use crate::util::{determine_wm, dmenu, WM};
+use crate::{
+    system_atlas::SYSTEM_ATLAS,
+    util::{determine_wm, dmenu, WM},
+};
 
-fn get_layout_names_hyprland(sh: &Shell) -> anyhow::Result<Vec<String>> {
-    // The hyprland library does not allow us to query the list of registered keyboard layouts, and
-    // we cannot do it through the hyprctl cli either - we have to parse the config file
+/// The hyprland library does not allow us to query the list of registered keyboard layouts, and
+/// we cannot do it through the hyprctl cli either - we have to parse the config file.
+/// For now, we care only about the layout, and not the variant or the options, because we only
+/// have one of each layout.
+/// This is a _very_ naive implementation! I could make this more elaborate, but it would make
+/// more sense to contribute to the hyprland library instead.
+fn get_layout_names_hyprland() -> anyhow::Result<Vec<String>> {
+    println!("{}", SYSTEM_ATLAS.hyprland);
+    let hyprland_config = File::open(SYSTEM_ATLAS.hyprland)?;
+    let reader = BufReader::new(hyprland_config);
+
+    for line in reader.lines() {
+        let Ok(line) = line else {
+            continue;
+        };
+
+        let line_trimmed = line.trim();
+        if !line_trimmed.starts_with("kb_layout") {
+            continue;
+        }
+
+        let Some((_, layouts)) = line_trimmed.split_once('=') else {
+            // This is fine; maybe we found the word we're looking for in a comment
+            // (not very probable in this case though...)
+            continue;
+        };
+
+        return Ok(layouts
+            .split(',')
+            .map(|l| l.trim().to_owned())
+            .collect::<Vec<_>>());
+    }
+
+    Err(anyhow::anyhow!(
+        "Did not find definitions for keyboard layouts in the Hyprland configuration file!"
+    ))
 }
 
 fn get_layout_names(sh: &Shell, wm: WM) -> anyhow::Result<Vec<String>> {
-    unimplemented!()
+    match wm {
+        WM::Hyprland => get_layout_names_hyprland(),
+        _ => unimplemented!(),
+    }
 }
 
 fn set_layout(sh: &Shell, layout_index: usize, wm: WM) -> anyhow::Result<()> {
--- src/system_atlas.rs
+++ src/system_atlas.rs
@@ -1,3 +1,5 @@
+/// Collection of system paths which we are interested about.
+/// NOTE: File::open does not expand '~', so it's safer to specify the full path!
 pub struct SystemAtlas<'a> {
     pub alacritty: &'a str,
     pub fontconfig: &'a str,
@@ -5,13 +7,15 @@ pub struct SystemAtlas<'a> {
     pub eww_gamma: &'a str,
     pub eww_volume: &'a str,
     pub eww_workspaces: &'a str,
+    pub hyprland: &'a str,
 }
 
 pub const SYSTEM_ATLAS: SystemAtlas = SystemAtlas {
-    alacritty: "~/.config/alacritty/alacritty.yaml",
-    fontconfig: "~/.config/fontconfig/fonts.conf",
+    alacritty: "/home/rg/.config/alacritty/alacritty.yaml",
+    fontconfig: "/home/rg/.config/fontconfig/fonts.conf",
     eww_brightness: "/tmp/eww-brightness",
     eww_gamma: "/tmp/eww-gamma",
     eww_volume: "/tmp/eww-volume",
     eww_workspaces: "/tmp/eww-workspaces",
+    hyprland: "/home/rg/.config/hypr/hyprland.conf",
 };
