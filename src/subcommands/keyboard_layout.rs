1d68acdf10cb0c2840eff915950f8bfa074b1a7d
sij1nk <gergo.rajnavolgyi@gmail.com>
2024-01-18 21:13:56 +0100

    stash

--- src/subcommands/keyboard_layout.rs
+++ src/subcommands/keyboard_layout.rs
@@ -8,6 +8,13 @@ use crate::{
     util::{determine_wm, dmenu, WM},
 };
 
+fn get_layout_names_hyprland(sh: &Shell) -> anyhow::Result<Vec<String>> {
+    // The hyprland library does not allow us to query the list of registered keyboard layouts, and
+    // we cannot do it through the hyprctl cli either - we have to parse the config file
+    let hyprland_config_file = format!("{}/hypr", env!("XDG_CONFIG_HOME"))
+
+}
+
 fn get_layout_names(sh: &Shell, wm: WM) -> anyhow::Result<Vec<String>> {
     unimplemented!()
 }
@@ -20,19 +27,28 @@ pub fn command_extension(cmd: Command) -> Command {
     cmd
 }
 
+fn print_layout_names(layout_names: &[String]) {
+    println!("Found keyboard layouts:");
+    for layout_name in layout_names {
+        println!("{}", layout_name);
+    }
+}
+
 pub fn run(sh: &Shell, _: &ArgMatches, _: &SystemAtlas) -> anyhow::Result<()> {
     let wm = determine_wm();
     let layout_names = get_layout_names(sh, wm)?;
 
+    print_layout_names(&layout_names);
+
     // unwrap: don't want to continue if string is empty
-    let result_index_str = dmenu(sh, "Choose keyboard layout", &layout_names, true).unwrap();
-
-    // unwrap: split always return at least 1 element
-    let result_index_str = result_index_str.split(':').next().unwrap();
-
-    let result_index = usize::from_str(result_index_str)?;
-
-    set_layout(sh, result_index, wm)?;
+    // let result_index_str = dmenu(sh, "Choose keyboard layout", &layout_names, true).unwrap();
+    //
+    // // unwrap: split always return at least 1 element
+    // let result_index_str = result_index_str.split(':').next().unwrap();
+    //
+    // let result_index = usize::from_str(result_index_str)?;
+    //
+    // set_layout(sh, result_index, wm)?;
 
     // TODO: notify the system bar (eww in our case)
 
