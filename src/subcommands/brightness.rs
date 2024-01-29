6f62ca28da9607723b35b83a8be41c747c4816e6
sij1nk <gergo.rajnavolgyi@gmail.com>
2024-01-29 21:40:25 +0100

    Fully implement brightness subcmd

--- src/subcommands/brightness.rs
+++ src/subcommands/brightness.rs
@@ -1,10 +1,12 @@
+use std::fs::OpenOptions;
+use std::io::{LineWriter, Write};
 use std::time::Duration;
 
 use clap::{arg, value_parser, ArgMatches, Command, ValueEnum};
-use dbus::blocking::Connection;
+use dbus::{arg, blocking::Connection};
 use xshell::Shell;
 
-use crate::system_atlas::SystemAtlas;
+use crate::system_atlas::SYSTEM_ATLAS;
 
 #[derive(ValueEnum, Clone, Debug)]
 enum Direction {
@@ -25,7 +27,7 @@ pub fn command_extension(cmd: Command) -> Command {
     )
 }
 
-pub fn run(sh: &Shell, args: &ArgMatches, atlas: &SystemAtlas) -> anyhow::Result<()> {
+fn determine_delta(args: &ArgMatches) -> anyhow::Result<f64> {
     let direction = args
         .get_one::<Direction>("direction")
         .expect("Direction argument is required");
@@ -38,9 +40,36 @@ pub fn run(sh: &Shell, args: &ArgMatches, atlas: &SystemAtlas) -> anyhow::Result
         Direction::Down => -delta as f64 / 100.0,
     };
 
+    Ok(signed_delta)
+}
+
+fn write_brightness_to_backing_file(brightness: f64) -> anyhow::Result<()> {
+    let file = OpenOptions::new()
+        .create(false)
+        .append(true)
+        .open(SYSTEM_ATLAS.eww_brightness)?;
+    let mut writer = LineWriter::new(&file);
+    writeln!(writer, "{}", brightness.round())?;
+
+    Ok(())
+}
+
+pub fn run(_: &Shell, args: &ArgMatches) -> anyhow::Result<()> {
+    let signed_delta = determine_delta(args)?;
+
     let connection = Connection::new_session()?;
     let proxy = connection.with_proxy("rs.wl-gammarelay", "/", Duration::from_secs(1));
+
     proxy.method_call("rs.wl.gammarelay", "UpdateBrightness", (signed_delta,))?;
+    use dbus::blocking::stdintf::org_freedesktop_dbus::Properties;
+
+    let brightness_refarg: Box<dyn arg::RefArg> = proxy.get("rs.wl.gammarelay", "Brightness")?;
+    let brightness = 100.0
+        * brightness_refarg.as_f64().ok_or(anyhow::anyhow!(
+            "rs.wl.gammarelay.Brightness is not an f64 value"
+        ))?;
+
+    write_brightness_to_backing_file(brightness)?;
 
     Ok(())
 }
