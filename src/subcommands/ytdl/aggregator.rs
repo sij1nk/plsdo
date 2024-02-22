549fdb17ad6509a30ec43d17f3f761a9c617ee1f
sij1nk <gergo.rajnavolgyi@gmail.com>
2024-02-22 08:58:37 +0100

    WIP: ytdl progress

--- src/subcommands/ytdl/aggregator.rs
+++ src/subcommands/ytdl/aggregator.rs
@@ -4,7 +4,7 @@ use std::{
     sync::{Arc, Mutex},
 };
 
-use super::{DownloadMetadata, DownloadProgress};
+use super::{message::DownloadProgress, DownloadMetadata};
 
 // TODO: serialization over the unix socket (serde? some binary format or json?)
 // TODO: prepare progress server to be a systemd service? logging and stuff?
@@ -14,14 +14,8 @@ const SOCKET_PATH: &str = "/tmp/plsdo-ytdl-progress-server.sock";
 
 type ProcessId = u32;
 
-enum Message {
-    BeginDownload(ProcessId, DownloadMetadata),
-    ReportDownloadProgress(ProcessId, DownloadProgress),
-    GetDownloadProgress,
-}
-
 /// Launch a daemon process, which maintains a map of ongoing ytdl downloads
-pub fn run_progress_server() -> std::io::Result<()> {
+pub fn run() -> std::io::Result<()> {
     let download_progress_map: Arc<
         Mutex<BTreeMap<ProcessId, (DownloadMetadata, DownloadProgress)>>,
     >;
---
+++ ytdl-run.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+cargo run -- ytdl download url "https://www.youtube.com/watch?v=EN6fvm5B0-s"
