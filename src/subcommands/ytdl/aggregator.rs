sij1nk/plsdo

commit 66d41ce22e39fe2161fe7383d62ed3c996083284

Remove MessageSender trait
my intention with this was to allow creating a mock/dummy MessageSender
impl for testing ytdlline -> message conversion, but I realized that's
unnecessary, since it's covered by ytdlline unit tests

src/subcommands/ytdl/mod.rs
@@ -83,40 +83,27 @@ fn get_download_url(download_args: &ArgMatches) -> anyhow::Result<String> {
     }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 struct Message {
     pid: u32,
     payload: MessagePayload,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 enum MessagePayload {
     YtdlLine(YtdlLine),
     ProcessExited(i32),
 }
 
-trait MessageSender {
-    fn send_message(&mut self, message: &Message) -> anyhow::Result<()>;
+fn send_message(stream: &UnixStream, message: &Message) -> anyhow::Result<()> {
+    serde_json::to_writer(stream, message)?;
+    Ok(())
 }
 
-struct AggregatorMessageSender;
-
-impl MessageSender for AggregatorMessageSender {
-    fn send_message(&mut self, message: &Message) -> anyhow::Result<()> {
-        let stream = UnixStream::connect(SYSTEM_ATLAS.ytdl_aggregator_socket)?;
-        serde_json::to_writer(stream, message)?;
-        Ok(())
-    }
-}
-
-// TODO:
-struct DummyMessageSender;
-
-// TODO: maybe map from lines -> messages and test that instead?
 fn process_lines(
     pid: u32,
+    stream: &UnixStream,
     lines: impl Iterator<Item = std::io::Result<String>>,
-    sender: &mut impl MessageSender,
 ) {
     for line in lines {
         match line {
@@ -126,7 +113,7 @@ fn process_lines(
                         pid,
                         payload: MessagePayload::YtdlLine(line),
                     };
-                    if let Err(err) = sender.send_message(&message) {
+                    if let Err(err) = send_message(stream, &message) {
                         eprintln!("Could not send message!");
                         eprintln!("{:?}", err);
                     }
@@ -153,8 +140,8 @@ fn download(sh: &Shell, download_args: &ArgMatches) -> anyhow::Result<()> {
     let stdout = child.stdout.take().expect("Child should have stdout");
     let bufreader = BufReader::new(stdout);
     let pid = child.id();
-    let mut sender = AggregatorMessageSender;
-    process_lines(pid, bufreader.lines(), &mut sender);
+    let stream = UnixStream::connect(SYSTEM_ATLAS.ytdl_aggregator_socket)?;
+    process_lines(pid, &stream, bufreader.lines());
 
     let ecode = child.wait().expect("wait on child failed");
     let ecode = ecode.code().unwrap_or(1);
@@ -162,7 +149,7 @@ fn download(sh: &Shell, download_args: &ArgMatches) -> anyhow::Result<()> {
         pid,
         payload: MessagePayload::ProcessExited(ecode),
     };
-    let _ = sender.send_message(&message);
+    let _ = send_message(&stream, &message);
     Ok(())
 }
 

commit ced2ecf0febbe56fa99f84d867b8a2713bfd6d26

WIP: break message sending
- aggregator complains of trailing characters when parsing first
  incoming message
- didn't this work before?
- tried including \n or \0 after each message str when sending, but
  read_to_string only exists when the socket breaks?
- broken by 66d41ce
- previously we were creating separate connections to the unix socket
  per message!!!
- test!!!

src/subcommands/ytdl/aggregator.rs
@@ -1,5 +1,6 @@
 use std::{
     collections::BTreeMap,
+    io::Read,
     os::unix::net::UnixListener,
     path::Path,
     sync::{Arc, Mutex},
@@ -192,9 +193,13 @@ pub fn run() -> anyhow::Result<()> {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(stream) => {
-                let message: Message = serde_json::from_reader(stream)?;
-                let _ = process_message(&state, message);
+            Ok(mut stream) => {
+                let mut str = String::new();
+                let _ = stream.read_to_string(&mut str)?;
+                println!("{str}");
+
+                // let message: Message = serde_json::from_reader(stream)?;
+                // let _ = process_message(&state, message);
             }
             Err(err) => break,
         }

src/subcommands/ytdl/mod.rs
@@ -1,7 +1,7 @@
 use clap::{arg, value_parser, ArgMatches, Command, ValueEnum};
 use serde::{Deserialize, Serialize};
 use std::{
-    io::{BufRead, BufReader},
+    io::{BufRead, BufReader, Read, Write},
     os::unix::net::UnixStream,
     process::{Command as StdCommand, Stdio},
 };
@@ -102,9 +102,19 @@ enum MessagePayload {
     ProcessExited(i32),
 }
 
-fn send_message(stream: &UnixStream, message: &Message) -> anyhow::Result<()> {
-    serde_json::to_writer(stream, message)?;
+fn send_message(mut stream: &UnixStream, message: &Message) -> anyhow::Result<()> {
+    let mut string = serde_json::to_string(message)?;
+    string.push('\0');
+    stream.write_all(string.as_bytes())?;
+    // serde_json::to_writer(stream, message)?;
     Ok(())
+}
+
+fn send_query_message(mut stream: &UnixStream, message: &Message) -> anyhow::Result<String> {
+    send_message(stream, message)?;
+    let mut response = String::new();
+    stream.read_to_string(&mut response)?;
+    Ok(response)
 }
 
 fn process_lines(
@@ -148,7 +158,13 @@ fn download(sh: &Shell, download_args: &ArgMatches) -> anyhow::Result<()> {
     let format_specifier = get_download_format_specifier(&format);
 
     let mut child = StdCommand::new("yt-dlp")
-        .args([format_specifier, &["--progress", "--newline", &url]].concat())
+        .args(
+            [
+                format_specifier,
+                &["--progress", "--newline", "-r", "16384", &url],
+            ]
+            .concat(),
+        )
         .stdout(Stdio::piped())
         .spawn()
         .expect("it to work");
@@ -172,7 +188,12 @@ pub fn run(sh: &Shell, args: &ArgMatches) -> anyhow::Result<()> {
     match args.subcommand() {
         Some(("download", download_args)) => download(sh, download_args)?,
         Some(("run_aggregator", _)) => aggregator::run()?,
-        Some(("get_download_progress", _)) => {}
+        Some(("get_download_progress", _)) => {
+            let message = Message::QueryMessage;
+            let stream = UnixStream::connect(SYSTEM_ATLAS.ytdl_aggregator_socket)?;
+            let response = send_query_message(&stream, &message)?;
+            println!("{response}");
+        }
         _ => {}
     }
 