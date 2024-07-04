fe37d0c039a38bb7508cc2189c0d7d13a8410869
sij1nk <gergo.rajnavolgyi@gmail.com>
2024-07-04 21:32:14 +0200

    Refactor into Dmenu trait

--- src/subcommands/game.rs
+++ src/subcommands/game.rs
@@ -1,21 +1,23 @@
 use clap::{arg, ArgMatches, Command};
 use xshell::{cmd, Shell};
 
-use crate::util::dmenu;
+use crate::util::dmenu::get_platform_dmenu;
 
 pub fn command_extension(cmd: Command) -> Command {
     cmd.arg(arg!([GAME]))
 }
 
 pub fn run(sh: &Shell, args: &ArgMatches) -> anyhow::Result<Option<String>> {
+    let dmenu = get_platform_dmenu();
     let list_output = cmd!(sh, "lutris -l").ignore_stderr().read()?;
     let mut choices = list_output
         .split('\n')
         .map(|s| s.split('|').take(2).collect::<Vec<_>>().join("|"))
         .collect::<Vec<_>>();
     choices.sort();
+    let choices_str = choices.iter().map(|e| e.as_ref()).collect::<Vec<&str>>();
 
-    let mut filtered_choices = choices.clone();
+    let mut filtered_choices = choices_str.clone();
     let search = "";
     if let Some(search) = args.get_one::<String>("GAME") {
         filtered_choices.retain(|name| name.contains(search));
@@ -26,18 +28,21 @@ pub fn run(sh: &Shell, args: &ArgMatches) -> anyhow::Result<Option<String>> {
     // If there is only one result, there's no point in showing dmenu;
     // game should be launched directly
     if filtered_choices.len() == 1 {
-        result = filtered_choices.remove(0);
+        result = filtered_choices.remove(0).to_owned();
     } else if filtered_choices.is_empty() {
-        result = dmenu(
-            sh,
-            &format!("Choose game (no matches found for '{search}')"),
-            &choices,
-            true,
-        )
-        .unwrap();
+        result = dmenu
+            .choose_one(
+                sh,
+                &format!("Choose game (no matches found for '{search}')"),
+                &choices_str,
+                true,
+            )
+            .unwrap();
     } else {
         // unwrap: we don't want to continue if result is empty
-        result = dmenu(sh, "Choose game", &filtered_choices, true).unwrap();
+        result = dmenu
+            .choose_one(sh, "Choose game", &filtered_choices, true)
+            .unwrap();
     }
 
     // unwrap: result always contains a pipe, and the first element is always a number
---
+++ src/util/dmenu.rs
@@ -0,0 +1,88 @@
+use anyhow::Context;
+use xshell::{cmd, Shell};
+
+fn is_wayland_session() -> bool {
+    std::env::vars()
+        .find(|(k, _)| k == "XDG_SESSION_TYPE")
+        .map(|(_, v)| v == "wayland")
+        .unwrap_or(false)
+}
+
+pub fn get_platform_dmenu() -> Box<dyn Dmenu> {
+    if is_wayland_session() {
+        Box::new(WaylandDmenu {})
+    } else {
+        Box::new(X11Dmenu {})
+    }
+}
+
+pub trait Dmenu {
+    fn choose_one(
+        &self,
+        sh: &Shell,
+        prompt: &str,
+        choices: &[&str],
+        forbid_invalid: bool,
+    ) -> anyhow::Result<String>;
+}
+
+pub struct WaylandDmenu;
+
+impl Dmenu for WaylandDmenu {
+    fn choose_one(
+        &self,
+        sh: &Shell,
+        prompt: &str,
+        choices: &[&str],
+        forbid_invalid: bool,
+    ) -> anyhow::Result<String> {
+        let choices_joined = choices.join("\n");
+        let lines = choices.len();
+        let lines_str = format!("{lines}");
+
+        let chosen = cmd!(sh, "bemenu -l {lines_str} --prompt {prompt}")
+            .stdin(&choices_joined)
+            .read()
+            .map_err(anyhow::Error::new);
+
+        let chosen = chosen.context("Aborted")?;
+
+        if forbid_invalid && !choices_joined.contains(&chosen) {
+            anyhow::bail!("Invalid input given");
+        }
+
+        Ok(chosen)
+    }
+}
+
+struct X11Dmenu;
+
+impl Dmenu for X11Dmenu {
+    fn choose_one(
+        &self,
+        sh: &Shell,
+        prompt: &str,
+        choices: &[&str],
+        forbid_invalid: bool,
+    ) -> anyhow::Result<String> {
+        let choices_joined = choices.join("\n");
+        let lines = choices.len().min(10);
+        let lines_str = format!("{lines}");
+
+        let chosen: anyhow::Result<String> = cmd!(
+            sh,
+            "dmenu -p {prompt} -i -l {lines_str} -fn 'monospace:size=24'"
+        )
+        .stdin(&choices_joined)
+        .read()
+        .map_err(anyhow::Error::new);
+
+        let chosen = chosen.context("Aborted")?;
+
+        if forbid_invalid && !choices_joined.contains(&chosen) {
+            return Err(anyhow::anyhow!("Invalid input given"));
+        }
+
+        Ok(chosen)
+    }
+}
